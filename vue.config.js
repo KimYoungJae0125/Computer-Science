cadbbd152244c02f1512ff8dd696b2819cd475cf
kyj <uni0125@naver.com>
2023-04-12 19:01:59 +0900

    Modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'prod' ? './Computer-Science/' : "./",
+  publicPath: process.env.NODE_ENV === 'production' ? './Computer-Science/' : "./",
   assetsDir: "./"
 })
