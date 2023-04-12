a49701b03c6cd41159b292863db6875fb59b5637
kyj <uni0125@naver.com>
2023-04-12 12:24:28 +0900

    Test github actions

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      테스트 깃허브 액션
+    </h1>
   </div>
 </template>
 
