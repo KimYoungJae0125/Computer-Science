a956ac91adbeed00e375e64e135eb1d723ca9438
kyj <uni0125@naver.com>
2023-04-20 18:11:43 +0900

    modify descriptions

--- src/components/dataStructure/DirectedGraphComponent.vue
+++ src/components/dataStructure/DirectedGraphComponent.vue
@@ -10,10 +10,10 @@ import DescriptionComponent from '../DescriptionComponent.vue';
 import DirectedGraphCanvas from '@/canvas/graph/DirectedGraphCanvas.vue';
 
 const descriptions = [
-    '무방향 그래프(Undirected Graph)는 두 정점을 연결하는 간선에 방향이 없는 그래프',
-    '무방향 그래프에서 정점 Vi와 Vj를 연결하는 간선을 (Vi, Vj)로 표현하는데, 이 때 (Vi, Vj)와 (Vj, Vi)는 같은 간선을 나타냅니다.',
-    '무방향 그래프는 방향이 없기 때문에 (A -> B) == (B -> A) 가 성립된다.',
-    'V(G1)={A, B, C, D}, E(G1)={(A, B), (A, D), (B, C), (B, D), (C, D)}',
+    '방향 그래프(Directed Graph)는 간선에 방향이 있는 그래프',
+    '방향 그래프에서 정점 Vi와 Vj를 연결하는 간선을 <Vi, Vj>로 표현하는데, 이 때 Vi를 꼬리(tail), Vj를 머리(head)라고 한다.',
+    '방향 그래프는 <Vi, Vj> 와 <Vj, Vi>는 서로 다른 간선',
+    'V(G1)={A, B, C, D}, E(G1)={(A, B), (B, C), (C, D), (D, A)}',
 ]
 
 export default {
--- src/components/dataStructure/UndirectedGraphComponent.vue
+++ src/components/dataStructure/UndirectedGraphComponent.vue
@@ -13,7 +13,7 @@ const descriptions = [
     '무방향 그래프(Undirected Graph)는 두 정점을 연결하는 간선에 방향이 없는 그래프',
     '무방향 그래프에서 정점 Vi와 Vj를 연결하는 간선을 (Vi, Vj)로 표현하는데, 이 때 (Vi, Vj)와 (Vj, Vi)는 같은 간선을 나타냅니다.',
     '무방향 그래프는 방향이 없기 때문에 (A -> B) == (B -> A) 가 성립된다.',
-    'V(G1)={A, B, C, D}, E(G1)={(A, B), (A, D), (B, C), (B, D), (C, D)}',
+    'V(G1)={A, B, C, D}, E(G1)={(A, B), (A, D), (B, C), (C, D)}',
 ]
 
 export default {
